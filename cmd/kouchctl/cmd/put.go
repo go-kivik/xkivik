bd33933a306d6aca45ef033e15a1de3c9695bb07
Jonathan Hall <flimzy@flimzy.com>
2020-12-02 13:06:17 +0100

    Add 'put attachment' command

--- cmd/kouchctl/cmd/put.go
+++ cmd/kouchctl/cmd/put.go
@@ -32,7 +32,7 @@ type put struct {
 	file string
 	yaml bool
 
-	doc *cobra.Command
+	doc, att *cobra.Command
 
 	*root
 }
@@ -42,6 +42,7 @@ func putCmd(r *root) *cobra.Command {
 		root: r,
 	}
 	c.doc = putDocCmd(r, c)
+	c.att = putAttCmd(r, c)
 	cmd := &cobra.Command{
 		Use:   "put",
 		Short: "Put a resource",
@@ -52,6 +53,7 @@ func putCmd(r *root) *cobra.Command {
 	c.configFlags(cmd.PersistentFlags())
 
 	cmd.AddCommand(c.doc)
+	cmd.AddCommand(c.att)
 
 	return cmd
 }
@@ -65,6 +67,9 @@ func (c *put) configFlags(pf *pflag.FlagSet) {
 func (c *put) RunE(cmd *cobra.Command, args []string) error {
 	if c.conf.HasDoc() {
 		return c.doc.RunE(cmd, args)
+	}
+	if c.conf.HasAttachment() {
+		return c.att.RunE(cmd, args)
 	}
 	_, err := c.client()
 	if err != nil {
---
+++ cmd/kouchctl/cmd/put_attachment.go
@@ -0,0 +1,93 @@
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy of
+// the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations under
+// the License.
+
+package cmd
+
+import (
+	"io"
+	"io/ioutil"
+	"os"
+	"strings"
+
+	"github.com/go-kivik/kivik/v4"
+	"github.com/go-kivik/xkivik/v4/cmd/kouchctl/errors"
+	"github.com/spf13/cobra"
+)
+
+type putAttachment struct {
+	contentType string
+	*root
+	*put
+}
+
+func putAttCmd(r *root, p *put) *cobra.Command {
+	c := &putAttachment{
+		root: r,
+		put:  p,
+	}
+	cmd := &cobra.Command{
+		Use:     "attachment [dsn]/[database]/[document]/[filename]",
+		Aliases: []string{"att", "attach"},
+		Short:   "Put an attachment",
+		Long:    `Create or update the named attachment`,
+		RunE:    c.RunE,
+	}
+
+	f := cmd.Flags()
+	f.StringVarP(&c.contentType, "content-type", "T", "", "Content-Type type of the attachment")
+
+	return cmd
+}
+
+func (c *putAttachment) RunE(cmd *cobra.Command, _ []string) error {
+	client, err := c.client()
+	if err != nil {
+		return err
+	}
+	content, err := c.rawData()
+	if err != nil {
+		return err
+	}
+	db, docID, filename, err := c.conf.DBDocFilename()
+	if err != nil {
+		return err
+	}
+	c.log.Debugf("[put] Will put attachment: %s/%s/%s/%s", client.DSN(), db, docID, filename)
+	return c.retry(func() error {
+		att := &kivik.Attachment{
+			Filename:    filename,
+			ContentType: c.contentType,
+			Content:     content,
+		}
+		rev, err := client.DB(db).PutAttachment(cmd.Context(), docID, att, c.opts())
+		if err != nil {
+			return err
+		}
+		c.log.Info(rev)
+		return nil
+	})
+}
+
+func (c *putAttachment) rawData() (io.ReadCloser, error) {
+	if c.data != "" {
+		return ioutil.NopCloser(strings.NewReader(c.data)), nil
+	}
+	switch c.file {
+	case "-":
+		return os.Stdin, nil
+	case "":
+	default:
+		f, err := os.Open(c.file)
+		return f, errors.Code(errors.ErrNoInput, err)
+	}
+	return nil, errors.Code(errors.ErrUsage, "no attachment data provided")
+}
