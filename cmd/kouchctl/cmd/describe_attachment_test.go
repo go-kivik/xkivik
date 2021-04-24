b48ae8142b37f927dd79d454c8eaeda044b20a98
Jonathan Hall <flimzy@flimzy.com>
2021-04-24 15:25:38 +0200

    Update tests for Go 1.16

--- cmd/kouchctl/cmd/describe_attachment_test.go
+++ cmd/kouchctl/cmd/describe_attachment_test.go
@@ -49,9 +49,10 @@ func Test_describe_attachment_RunE(t *testing.T) {
 		s := testy.ServeResponse(&http.Response{
 			StatusCode: http.StatusOK,
 			Header: http.Header{
-				"Content-Type": []string{"text/plain"},
-				"Server":       []string{"CouchDB/2.3.1 (Erlang OTP/20)"},
-				"ETag":         []string{`"cy5z3SF7yaYp4vmLX0k31Q==`},
+				"Content-Type":   []string{"text/plain"},
+				"Server":         []string{"CouchDB/2.3.1 (Erlang OTP/20)"},
+				"ETag":           []string{`"cy5z3SF7yaYp4vmLX0k31Q==`},
+				"Content-Length": []string{"7"},
 			},
 			Body: ioutil.NopCloser(strings.NewReader(`Testing`)),
 		})
@@ -64,9 +65,10 @@ func Test_describe_attachment_RunE(t *testing.T) {
 		s := testy.ServeResponse(&http.Response{
 			StatusCode: http.StatusOK,
 			Header: http.Header{
-				"Content-Type": []string{"text/plain"},
-				"Server":       []string{"CouchDB/2.3.1 (Erlang OTP/20)"},
-				"ETag":         []string{`"cy5z3SF7yaYp4vmLX0k31Q==`},
+				"Content-Type":   []string{"text/plain"},
+				"Server":         []string{"CouchDB/2.3.1 (Erlang OTP/20)"},
+				"ETag":           []string{`"cy5z3SF7yaYp4vmLX0k31Q==`},
+				"Content-Length": []string{"7"},
 			},
 			Body: ioutil.NopCloser(strings.NewReader(`Testing`)),
 		})
